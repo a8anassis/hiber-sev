b0872babe37ba17d74d60442a08150b7d9a0ef1e
a8anassis <a8anassis@gmail.com>
2026-01-09 20:23:09 +0200

    Initial commit

---
+++ src/main/java/gr/aueb/sev/model/Teacher.java
@@ -0,0 +1,58 @@
+package gr.aueb.sev.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname", length = 255, nullable = false, unique = false)     // default values
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    public Teacher() { }
+
+    public Teacher(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
