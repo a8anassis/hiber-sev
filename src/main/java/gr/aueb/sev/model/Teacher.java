5a65b8a9e32310dcffc3e36074afe7ef53743c3b
a8anassis <a8anassis@gmail.com>
2026-01-09 21:02:53 +0200

    feat: add lombok to teacher

--- src/main/java/gr/aueb/sev/model/Teacher.java
+++ src/main/java/gr/aueb/sev/model/Teacher.java
@@ -1,11 +1,16 @@
 package gr.aueb.sev.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "teachers")
 public class Teacher {
@@ -20,51 +25,52 @@ public class Teacher {
     @Column(nullable = false)
     private String lastname;
 
+    @Getter(AccessLevel.PROTECTED)
     @OneToMany(mappedBy = "teacher", cascade = CascadeType.PERSIST) // best practice
     private Set<Course> courses = new HashSet<>();
 
-    public Teacher() { }
-
-    public Teacher(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    protected Set<Course> getCourses() {
-        return courses;
-    }
-
+//    public Teacher() { }
+//
+//    public Teacher(String firstname, String lastname) {
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//
+//    protected Set<Course> getCourses() {
+//        return courses;
+//    }
+//
     public Set<Course> getAllCourses() {
         return Collections.unmodifiableSet(courses);
     }
-
-    public void setCourses(Set<Course> courses) {
-        this.courses = courses;
-    }
+//
+//    public void setCourses(Set<Course> courses) {
+//        this.courses = courses;
+//    }
 
     public void addCourse(Course course) {
         courses.add(course);
