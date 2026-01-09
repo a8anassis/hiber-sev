eaf2f515a131b9af86677f6c91cad14b3e3c5d54
a8anassis <a8anassis@gmail.com>
2026-01-09 20:26:01 +0200

    feat: add Course entity

---
+++ src/main/java/gr/aueb/sev/model/Course.java
@@ -0,0 +1,46 @@
+package gr.aueb.sev.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    public Course() { }
+
+    public Course(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
